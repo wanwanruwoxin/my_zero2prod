15d61b0711d0ffe240cc066222fcb60c420b3a8c
ql <254693270@qq.com>
2025-08-22 16:56:53 +0800

    :sparkles: 健康检查接口

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,17 @@ use axum::{
     routing::get,
     Router,
 };
+use axum::http::StatusCode;
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+    let app = Router::new()
+        .route("/health_check", get(health_check));
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:8000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
+}
+
+async fn health_check() -> StatusCode {
+    StatusCode::OK
 }
