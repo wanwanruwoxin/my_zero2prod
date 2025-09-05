a3de3c893323c55b9b7a1b9908945ae3b9705764
ql <254693270@qq.com>
2025-09-05 09:54:47 +0800

    🍻 设置 tracing 的订阅，代替 env_log，使用 json 格式化输出日志

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,23 @@
-use env_logger::Env;
 use my_zero2prod::{configuration::get_configuration, startup::run};
 use sea_orm::{Database, DatabaseConnection};
 use tokio::net::TcpListener;
+use tracing::subscriber::set_global_default;
+use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
+use tracing_subscriber::{layer::SubscriberExt, EnvFilter, Registry};
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    env_logger::Builder::from_env(Env::default().default_filter_or("trace")).init();
+    // 日志过滤层
+    let env_filter = EnvFilter::try_from_default_env()
+        .unwrap_or_else(|_| EnvFilter::new("info"));
+    // 格式化层
+    let formatting_layer = BunyanFormattingLayer::new("zero2prod".into(), std::io::stdout);
+    // 创建订阅者
+    let subscriber = Registry::default()
+        .with(env_filter)
+        .with(JsonStorageLayer)
+        .with(formatting_layer);
+    set_global_default(subscriber).expect("设置 subscriber 失败");
 
     let configuration = get_configuration().expect("Failed to read configuration.");
     let db: DatabaseConnection = Database::connect(&configuration.database.connection_string())
