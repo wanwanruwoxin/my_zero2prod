61aa1ac16871e47ddaf918a8f79cac2ca6938a64
ql <254693270@qq.com>
2025-09-09 09:28:56 +0800

    🍻 新增数据库连接配置

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
+use std::time::Duration;
+
 use my_zero2prod::{
     configuration::get_configuration,
     startup::run,
     telemetry::{get_subscriber, init_subscriber},
 };
-use sea_orm::{Database, DatabaseConnection};
+use sea_orm::{ConnectOptions, Database};
 use secrecy::ExposeSecret;
 use tokio::net::TcpListener;
 
@@ -14,14 +16,22 @@ async fn main() -> std::io::Result<()> {
     init_subscriber(subscriber);
 
     let configuration = get_configuration().expect("Failed to read configuration.");
-    let db: DatabaseConnection = Database::connect(configuration.database.connection_string().expose_secret())
-        .await
-        .unwrap();
+    let mut opt = ConnectOptions::new(configuration.database.connection_string().expose_secret());
+    opt.max_connections(100)
+        .min_connections(5)
+        .connect_timeout(Duration::from_secs(2))
+        .acquire_timeout(Duration::from_secs(8))
+        .idle_timeout(Duration::from_secs(8))
+        .max_lifetime(Duration::from_secs(8))
+        .sqlx_logging(true)
+        .sqlx_logging_level(log::LevelFilter::Info);
+    let db = Database::connect(opt).await.unwrap();
 
-    let address = format!("{}:{}", configuration.application.host, configuration.application.port);
-    let listener = TcpListener::bind(address)
-        .await
-        .unwrap();
+    let address = format!(
+        "{}:{}",
+        configuration.application.host, configuration.application.port
+    );
+    let listener = TcpListener::bind(address).await.unwrap();
     run(listener, db).await;
     Ok(())
 }
